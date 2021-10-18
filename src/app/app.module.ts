ba3ae467c46eb5c7c520945ca625cf54b30e0d2e
wdmuthiora <wdmuthiora@gmail.com>
2021-10-18 11:27:07 +0300

    Added environment.ts file to .gitignore.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { ResultSectionComponent } from './components/result-section/result-section.component';
+import { ResultsComponent } from './components/results/results.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent,
+    ResultSectionComponent,
+    ResultsComponent,
+    LandingComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
