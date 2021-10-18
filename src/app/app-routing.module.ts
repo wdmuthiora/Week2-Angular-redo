b8661d1624f6995f224450d32f338382eea3ebe9
wdmuthiora <wdmuthiora@gmail.com>
2021-10-18 12:25:12 +0300

    Updated the routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResultsComponent } from './components/results/results.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { AboutComponent } from './components/about/about.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'results', component: ResultsComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'search', component: SearchFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
