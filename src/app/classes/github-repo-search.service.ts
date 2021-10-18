9d10afc848c9e2222f76ab0211ba8aa81267afe8
wdmuthiora <wdmuthiora@gmail.com>
2021-10-18 18:59:18 +0300

    Modified the githubRepoSearch service.

--- src/app/classes/github-repo-search.service.ts
+++ src/app/classes/github-repo-search.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GithubRepoSearchService {
+  private accessKey: string = environment.apiKey;
+  private baseApiURL: string = environment.baseURL;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getData(term: string): Observable<any> {
+    const endPoint = 'users';
+
+    let apiUrl = `${this.baseApiURL}/${endPoint}/${term}/repos`;
+
+    return this.http.get(apiUrl);
+  }
 }
