5766fe10fefcabd0f14d891db3aa2e54a6fdc00c
wdmuthiora <wdmuthiora@gmail.com>
2021-10-18 15:17:14 +0300

    Added route for not-found resources.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { ResultsComponent } from './components/results/results.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { AboutComponent } from './components/about/about.component';
 import { SearchFormComponent } from './components/search-form/search-form.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'results', component: ResultsComponent },
   { path: 'landing', component: LandingComponent },
   { path: 'about', component: AboutComponent },
+  // '**' The wildcards define a route that is not present in our routes array
+  { path: '**', component: NotFoundComponent },
   { path: 'search', component: SearchFormComponent },
 ];
 
