04ea373b656ef55e2598c5ddf3bedec69dbdb040
wdmuthiora <wdmuthiora@gmail.com>
2021-10-18 19:31:05 +0300

    Added loader.

--- src/app/services/github-repo-search.service.ts
+++ src/app/services/github-repo-search.service.ts
@@ -1,26 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import {environment} from '../../environments/environment' 
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
-  
-export class GithubSearchService {
-  
+export class GithubRepoSearchService {
   private accessKey: string = environment.apiKey;
-  private baseApiURL: string = environment.baseURL
+  private baseApiURL: string = environment.baseURL;
 
   constructor(private http: HttpClient) {}
 
-  getData(term:string): Observable<any> {
-
+  getData(term: string): Observable<any> {
     const endPoint = 'users';
-    // to get users
-    let apiUrl = `${this.baseApiURL}/${endPoint}/${term}`;
+
     // to get repos
-    // let apiUrl = `${this.baseApiURL}/${endPoint}/${term}/repos`;
+    let apiUrl = `${this.baseApiURL}/${endPoint}/${term}/repos`;
 
     return this.http.get(apiUrl);
   }
